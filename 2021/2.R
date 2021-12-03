07c345fbfb462b47ffe7d9a79bf27b05591e9303
Luca Valnegri <l.valnegri@datamaps.co.uk>
2021-12-03 00:55:42 +0000

    commit

--- 2021/2.R
+++ 2021/2.R
@@ -3,7 +3,7 @@ y <- fread('./2021/2.csv')
 
 y[, X1 := 0][V1 == 'forward', X1 := V2]
 y[, Y1 := ifelse(V1 == 'forward', 0, ifelse(V1 == 'down', V2, -V2))]
-sum(y$X) * sum(y$Y)
+sum(y$X1) * sum(y$Y1)
 
 y[, Z := ifelse(V1 == 'forward', 0, ifelse(V1 == 'down', V2, -V2))][, Z := cumsum(y[1:.N, Z])]
 y[, `:=`(X2 = 0, Y2 = 0)][V1 == 'forward', `:=`(X2 = V2, Y2 = V2 * Z)]
