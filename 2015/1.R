52e0cac7a0db6df86221d2df259de544d1041979
Luca Valnegri <l.valnegri@datamaps.co.uk>
2021-12-04 18:51:51 +0000

    commit

--- 2015/1.R
+++ 2015/1.R
@@ -3,5 +3,11 @@
 #####################################################################
 
 library(data.table)
-y <- fread('./2015/1.csv')
+y <- data.table(readLines('./2015/1.txt'))
+y <- y[, strsplit(V1, split = '')][, `:=`( M = 1:.N, V = ifelse(V1 == '(', 1, -1) )]
 
+# answer part 1
+y[, sum(V)]
+
+# answer part 2
+y[, S := cumsum(y[1:.N, V])][S < 0][1, M]
--- 2015/2.R
+++ 2015/2.R
@@ -3,5 +3,19 @@
 #####################################################################
 
 library(data.table)
-y <- fread('./2015/2.csv')
+y <- fread('./2015/2.csv', header = FALSE)
+y[, c('l', 'w', 'h') := tstrsplit(V1, split = 'x', type.convert = TRUE)
+  ][, `:=`( area = 2*l*w + 2*w*h + 2*h*l, slack = pmin(l*w, w*h, h*l) )
+    ][, total := area + slack]
+
+# answer part 1
+sum(y$total)
+
+# answer part 2
+mid_min <- Vectorize(function(l, w, h){
+  pm <- pmin(l, w, h)
+  ifelse(length(which(c(l, w, h) == pm)) > 1, pm, min( setdiff( c(l, w, h), pmin(l, w, h) ) ) )
+})
+y[, min1 := pmin(l, w, h)][, min2 := mid_min(l, w, h)][, ribbon := 2*(min1 + min2) + l*w*h]
+sum(y$ribbon)
 
