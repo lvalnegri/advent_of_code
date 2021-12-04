657e80f1c3a832a4d9afec7502caa8ec5b2f6b32
Luca Valnegri <l.valnegri@datamaps.co.uk>
2021-12-04 01:10:07 +0000

    got rid of `if`

--- 2021/3.R
+++ 2021/3.R
@@ -1,6 +1,5 @@
 library(data.table)
 y <- fread('./2021/3.csv', colClasses = 'character')
-
 y <- y[, tstrsplit(V1, split = '')]
 
 bin2int <- function(b){ strtoi( paste0( as.integer( b ), collapse = ''), base = 2 ) }
@@ -12,11 +11,11 @@ for(n in 0:1){
     yx <- copy(y)
     for(x in 1:ncol(y)){
         xv <- paste0('V', x)
-        if(n == 0){
-            yx <- yx[get(xv) == as.integer( yx[, sum(get(xv) == 1)] < yx[, sum(get(xv) == 0)] ) ]
-        } else {
-            yx <- yx[get(xv) == as.integer( yx[, sum(get(xv) == 1)] >= yx[, sum(get(xv) == 0)] ) ]
-        }
+        yx <- yx[get(xv) == eval(parse(text = paste( 
+                                    'as.integer( yx[, sum(get(xv) == 1)]', 
+                                    ifelse(n == 0, '<', '>=') , 
+                                    'yx[, sum(get(xv) == 0)] )' 
+        ))) ]
         if(nrow(yx) == 1) break
     }
     out[n + 1] <- bin2int(yx)
