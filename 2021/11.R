f4113387d546490ca3c7d1aa6ee8039c73be9803
Luca Valnegri <l.valnegri@datamaps.co.uk>
2021-12-11 19:20:54 +0000

    commit

--- 2021/11.R
+++ 2021/11.R
@@ -3,4 +3,37 @@
 #######################################################################
 
 library(data.table)
-y <- fread('./2021/11.csv')
+y <- as.matrix(fread('./2021/11.txt', header = FALSE)[, tstrsplit(V1, split = '')][, lapply(.SD, as.integer)])
+
+add_one <- function(y, i, j){
+    yx <- matrix(rep(FALSE, nrow(y) * ncol(y)), ncol = ncol(y))
+    yx[seq(max(0, i-1), min(nrow(y), i+1)), seq(max(0, j-1), min(ncol(y), j+1))] <- TRUE
+    y + yx
+}
+ps <- 0
+nf <- 0
+repeat({
+    ps <- ps + 1
+    y <- y + 1
+    yp <- matrix(rep(FALSE, nrow(y) * ncol(y)), ncol = ncol(y))
+    done <- FALSE
+    while(!done){
+        done <- TRUE
+        for(i in 1:nrow(y)){
+            for(j in 1:ncol(y)){
+                if(!yp[i, j] & y[i, j] > 9){
+                    y <- add_one(y, i, j)
+                    yp[i, j] <- TRUE
+                    nf <- nf + 1
+                    done <- FALSE
+                }
+            }
+        }
+    }
+    y[y > 9] <- 0
+    if(ps == 100) print(nf)
+    if(sum(y) == 0){
+        print(ps)
+        break
+    }
+})
